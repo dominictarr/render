16d0d8a8355a66bd2cf929bccf41a06ca5de1064
Dominic Tarr <dominic.tarr@gmail.com>
2011-01-21 11:22:36 +1300

    remove old version

--- render.js
+++ render.js
@@ -1,216 +1,83 @@
-//render
+//render2.js
+//a better renderer using traverser
 
-/*
-this could be refactored into a traverser...
-
-and then that used to make a renderer
-
-this is definately better than what i did in inspect though!
-
-instead make a traverser with methods like this:
-
-{ key:
-, object: //called on whole object, and can call cb to cancel.
-, primitive: (string,number,boolean)
-, typeof {string:, number:, boolean:, :function:, etc}
-, instanceof {String:, Object:, Array:, Date:, etc}
-
-pass everything the same args
-{ key:
-, value:
-, path (list of keys so far)
-, ancestors
-, circular (boolean)
-, referenced (boolean, if this object is referenced later)
-, reference (path to first occurance, or null) 
-, prune (stop exploring this branch)
-, halt (stop exploring tree) }
-
-*/
-
-var log = require('logger')
-  , style = require('style')
-  , curry = require('curry')
+var traverser = require('traverser')
+  
 exports = module.exports = render
 
-function render(obj){
-  return new Rendered(obj)
-}
+var defaults = {
+  indent: ''
+, pad: ' '
+, joiner: ', '
+, value: function (value,p){
+    if(p.value === undefined)
+      return 'undefined'
+    if('string' === typeof value)
+      return "\"" + value.split('\n').join('\n ') + "\""
 
-function Rendered(obj){
-  var self = this
-  self.obj = obj
-  self._format = []
-}
-
-function applyIf(func,args,def){
-  if ('function' === typeof func)
-    return func.apply(null,args)
-  return func == null ? def : func
-}
-
-Rendered.prototype = {
-  ol: function (){
-    var format = this._format
-    format.indent = ' '
-    format.brackets = ['','']
-    format.after = '\n'
-    format.before = ". "
-    format.between = ''
-    format.key = function (k){return k}
-    return this
-  },
-  render: function (obj,opts){
-      opts = opts || {path:[], indent: ''}
-      
-    var keys = Object.keys(obj)
-      , format = this._format
-      , after = format.after || ''
-      , before = format.before || ''
-      , brackets = applyIf(format.brackets,[obj,opts.path],['',''])
-      , self = this
-      , indent = opts.indent
-      , nextIndent = indent + (format.indent || '')
-
-      ,contents = 
-      
-     keys.map(function(key){
-      var _key = key
-      var line = ''
-      var value = obj[key]
-      var path = [].concat(opts.path).concat(_key)
-      if('object' === typeof value) {
-        key = applyIf(format.key,[key,opts.path,value,obj],key)
-        line = key + self.render(value, {path: path, indent: (indent + key + before).replace(/./g,' ')})
-      } else {
-        key = applyIf(format.key,[key,opts.path,value,obj],key)
-        value = applyIf(format.primitive,[value,path],value)
-        line = key + before + value + after
-      }
-      return line
-    }).join(format.between || '')
-
-   return [brackets[0], contents, brackets[1]].join('')
-  },
-  toString: function (){
-    return this.render(this.obj)
-  },
-  format : function (formatting){
-    this._format = formatting
-    return this
-  },
-  get renderer (){
+    return JSON.stringify(value)
+  }
+, key: function (key, p){
+    return p.parent instanceof Array ? '' : (/^\w+$/(key) ? key : "'" + key + "'") + ": "
+  }
+, join: function (lines,p,def){
     var self = this
-    return function (obj){
-      return self.render(obj)
-    }
+      , pad = lines.length ? self.pad : ''
+    return ( pad + 
+          lines.map 
+        ( function (e) {return indent(e, self.indent)} ).join (this.joiner) + pad)
+  }
+, reference: function (rendered,p){
+  return 'var' + p.index.repeated //+ "dfsdfdsf"
+}
+, referenced: function (index,p){
+   return 'var' + index + '='
+}
+, surround: function (objString,p){
+ 
+    if(p.value instanceof Date || p.value instanceof RegExp)
+      return p.value.toString()
+    if(p.value instanceof Array)
+      return '[' + objString + ']'
+    if(p.value === null)
+      return 'null'
+    if('function' == typeof p.value)
+      return  p.value.toString().replace(/{.+}$/m,'{...}')
+    return '{' + objString + '}'
   }
 }
-/*
-THE FOLLOWING IS REAL UGLY!
-*/
-
-var enabled = true
-
-var styles = {
-  brackets: style().grey.styler
-, number: style().blue.styler
-, string: style().green.styler
-, 'boolean': style().yellow.styler
-, 'undefined': style().red.styler
-, key: style().magenta.bold.styler
-}
-
-var inspectFormat = 
-  { key:
-     function (key,path,value,obj){
-       if(obj instanceof Array)
-         return ''
-       return styles.key(key,enabled) + ': '
-     }
-   , between: ', '
-   , brackets: function (obj) {
-
-      if(obj instanceof Array){
-        return ['[',']']
-      } else if ('object' === typeof obj){
-        return ['{','}']
-      } else if ('function' === typeof obj)
-        var f = /(function \w* ?\(\w*\))/.exec(obj.toString())
-
-        var sp = Object.keys(obj).length > 0 ? ' ' : ''
-        return ['[' + f[1]  + sp,']']
-      return ['','']
-    }
-  , primitive:
-      function (value,path){
-          return styles[typeof value](JSON.stringify(value),enabled)
-     }
+function render (obj, options){
+  options = options || {}
+  if(options.multi){
+    options.indent = '  '
+    options.joiner = '\n, '
   }
 
-var inspector = render().format(inspectFormat)
-
-exports.inspect = function (obj,coloured){
-  enabled = !coloured
-  return inspector.render(obj)
-  enabled = false
-}
-var other
-var equalp = true
-function get(obj,path){
-  path.forEach(function (c){
-    obj = obj [c]
-  })
-  return obj
-}
-function check(self,other,path){
-  return get(self,path) == get(other,path)
-}
-
-var equalsFormat = 
-  { key:
-     function (key,path,value,obj){
-       if(obj instanceof Array)
-         return ''
-
-        var s = (get(other,[].concat(path).concat(key)) == null ? 'red' : 'green')
-      if (s == 'red') equalp = false
-       return style(key)[s] + ': '
-     }
-   , between: ', '
-   , brackets: function (obj,path) {
-      if(obj instanceof Array){
-        return ['[',']']
-      } else if ('object' === typeof obj){
-        return ['{','}']
-      } else if ('function' === typeof obj)
-        var f = /(function \w* ?\(\w*\))/.exec(obj.toString())
-
-        var sp = Object.keys(obj).length > 0 ? ' ' : ''
-        return ['[' + f[1]  + sp,']']
-      return ['','']
-    }
-  , primitive:
-      function (value,path){
-        var s = (value != get(other,path) ? 'red' : 'green')
-
-      if (s == 'red') equalp = false
-        return style(JSON.stringify(value))[s]
-     }
-  }
-
-var checker = render().format(equalsFormat)
-
-exports.equals = function (_eqA,_eqB){
-  equalp = true
-  other = _eqB
-  var a = checker.render(_eqA) 
-  var ap = equalp
-  equalp = true
-  other = _eqA
-  var b = checker.render(_eqB) 
-  var bp = equalp
+    options.__proto__ = defaults
+  return traverser(obj, {branch: branch, leaf: leaf, isBranch:isBranch, pre:true})
   
-  return a + (ap && bp ? ' == ' : ' != ') + b
+  function isBranch(p){
+    return ('function' == typeof p.value || 'object' == typeof p.value)
+  }
+  function branch (p){
+    var key = (p.parent ? call('key',p.key,p) : '')    
+  
+    if(p.reference){
+     var r = call('reference',p.index.seen,p)
+      if(r !== undefined) return key + r
+    }
+    return key + (p.referenced ? call('referenced',p.index.repeated,p) : '') + 
+      call('surround',call('join',p.map(),p),p)
+  }
+  function leaf (p){
+    return (p.parent ? call('key',p.key,p) : '') + call('value',p.value,p) 
+  }
+  function call(method,value,p){
+    return options[method](value,p,options.__proto__[method])
+  }
+}
+
+function indent (s, ch){
+    return s.split('\n').join('\n' + ch)
 }
 
--- test/render.expresso.js
+++ test/render.expresso.js
@@ -1,168 +1,61 @@
 
-var render = require('render')
-  , describe = require('should').describe
+var it = require('it-is')
+  , render = require('../render')
   , log = require('logger')
-  , inspect = require('inspect')
-exports ['can render array as list'] = function (test){
-  var list = ['a','b','c']
-    , rendered = render(list).ol()
-    , it = 
-      describe('' + rendered,"rendered ordered list")
-    it.should.eql("0. a\n1. b\n2. c\n")
-    
-  log('' + rendered)
+  
+exports ['renders an object'] = function (test){
+  var a,b,c
+  c = []
+  c.push(c)
+  var examples =
+  [ [ [1,2,3,4], '[ 1, 2, 3, 4 ]']
+  , [ {}, /^{}$/]
+  , [ {a:'b'}, '{ a: "b" }']
+  , [ d = new Date, d.toString()]
+  , [ /xyz/, '/xyz/']
+  , [ null, 'null']
+  , [ undefined, 'undefined']
+  , [ c, 'var0=[ var0 ]']
+  , [ function a (x,y,z){}, 'function a(x,y,z){}']
+  , [ function (){'blah blah blah'}, 'function (){...}']
+  , [ {X: function (){'blah blah blah'} }, '{ X: function (){...} }']
+  ]
 
+//thats a basics. multilines and indentation.
+
+  it(examples).every(function (e){
+    if('string' == typeof e[1])
+      it(render(e[0])).equal(e[1])
+    else
+      it(render(e[0])).matches(e[1])
+  })
 }
 
-exports ['can render array same as Array.toString()'] = function (test){
-  var list = ['a','b','c']
-    , rendered = render(list).format({between:',',  key:''})
-    , it = 
-      describe('' + rendered,"rendered list with square brackets and ', '")
-    it.should.eql(list.toString())
-  log('' + rendered)
+exports ['multiline and indentation'] = function (test) {
+   var a,b,c
+  c = []
+  c.push(c)
+
+  var examples =
+  [ [ [1,2,3,4], "[ 1\n, 2\n, 3\n, 4 ]"]
+  , [ [1,2,[3,4]], "[ 1\n, 2\n, [ 3\n  , 4 ] ]"]
+  , [ [1,2,[[[]]],[3,4]], "[ 1\n, 2\n, [ [ [] ] ]\n, [ 3\n  , 4 ] ]"]
+  , [ [1,2,{x:'asdfasfasf',y:{K:21434}},[3,4]]
+    , "[ 1\n, 2\n, { x: \"asdfasfasf\"\n  , y: { K: 21434 } }\n, [ 3\n  , 4 ] ]" ]
+  , [ {}, '{}']
+  , [ [1,2,"hello\nthere",4] 
+    , "[ 1\n, 2\n, \"hello\n   there\"\n, 4 ]" ]
+  , [ {a:'b'}, '{ a: "b" }']
+  , [ c, 'var0=[ var0 ]']
+  , [ function a (x,y,z){}, 'function a(x,y,z){}']
+  , [ function (){'blah blah blah'}, 'function (){...}']
+  ]
+
+//thats a basics. multilines and indentation.
+
+  it(examples).every(function (e){
+    var r = render(e[0],{multi:true})
+    log(r)
+    it(r).equal(e[1])
+  })
 }
-
-exports ['can render array same as JSON.stringify()'] = function (test){
-  var list = ['a','b','c']
-    , rendered = render(list).format({brackets: ['[',']'], between:',',  key:'', primitive: JSON.stringify})
-    , it = 
-      describe('' + rendered,"rendered list with square brackets and ', '")
-    it.should.eql(JSON.stringify(list))
-  log('' + rendered)
-}
-
-exports ['can render nested arrays with indentation'] = function (test){
-  var list = ['a','b',[10,20,30],'c']
-    , rendered = render(list).format({
-        before: ''
-      , between:'\n'
-      , indent: ''
-      , key: function (key,path,value){
-          if('object' === typeof value)
-            return ''
-          return [].concat(path).concat(key).join('.') + ' - '
-        } 
-      } )
-    , it = 
-      describe('' + rendered,"rendered list with square brackets and ', '")
-  log('' + rendered)
-
-    it.should.eql(
-        '0 - a\n'
-      + '1 - b\n'
-      + '2.0 - 10\n'
-      + '2.1 - 20\n'
-      + '2.2 - 30\n'
-      + '3 - c' )
-}
-exports ['can be applied to any object'] = function (test){
-  var list = ['a','b',[10,20,30],'c']
-    , obj = {a: "AAA", b:"BBB", c: "CCC", d: [1,2,{'!':"wow!"}]}
-    , r = render().format({
-        before: ''
-      , between:'\n'
-      , indent: ''
-      , key: function (key,path,value){ //functions are much more useful than the other stuff...
-          if('object' === typeof value)
-            return ''
-          return [].concat(path).concat(key).join('.') + ' - '
-        } 
-      } )
-      
-  var rendered = '' + r.renderer(list)
-  log(rendered)
-   
-  var it =
-     describe(rendered, "rendered nested list with paths '")
-    it.should.eql(
-        '0 - a\n'
-      + '1 - b\n'
-      + '2.0 - 10\n'
-      + '2.1 - 20\n'
-      + '2.2 - 30\n'
-      + '3 - c' )
-
-  var rendered = '' + r.renderer(obj)
-  log(rendered)
-   
-  var it =
-     describe(rendered, "rendered nested list with paths '")
-
-  log('' + r.render(obj))
-    it.should.eql(
-      'a - AAA\n'
-    + 'b - BBB\n'
-    + 'c - CCC\n'
-    + 'd.0 - 1\n'
-    + 'd.1 - 2\n'
-    + 'd.2.! - wow!' )
-
-  var it =
-     describe(rendered, "rendered nested list with paths '")
-
-  log('' + r.render(obj))
-    it.should.eql(
-      'a - AAA\n'
-    + 'b - BBB\n'
-    + 'c - CCC\n'
-    + 'd.0 - 1\n'
-    + 'd.1 - 2\n'
-    + 'd.2.! - wow!' )
-}
-
-
-exports ['can render nested arrays same as inspect'] = function (test){
-  var list = ['a','b',[10,20,30],'c']
-    , obj = {a: "AAA", b:"BBB", c: "CCC", d: [1,2,{'!':"wow!"}]}
-    , func = function func(){}
-    , i = render.inspect
-      
-  var rendered = i(list)
-  log(i(list,true))
-   
-  var it =
-     describe(rendered, "rendered nested list, like inspect")
-    it.should.eql(inspect(list))
-
-
-  var rendered = i(obj)
-  log(i(obj,true))
-   
-  var it =
-     describe(rendered, "rendered nested object, like inspect")
-    it.should.eql(inspect(obj))
-
-  var rendered = i(func)
-  log(i(func,true))
-   
-  var it =
-     describe(rendered, "rendered nested object, like inspect")
-    it.should.eql(inspect(func))
-    
-  func.x = 'xxx'
-
-  var rendered = i(func)
-  log(i(func,true))
-  log(inspect(func))
-   
-  var it =
-     describe(rendered, "rendered nested object, like inspect")
-    it.should.eql(inspect(func))
-}
-
-exports ['can show how two objects are equal'] = function (test){
-/*
-  have this return an object and report what paths are unequal
-  that toString()s to the print out
-*/
-  log(render.equals([1,2,3],[1,2,4]))
-  log(render.equals([1,2,3],[1,2,3]))
-  log(render.equals([1,2,3],[1,2,3,'X']))
-  log(render.equals([1,2,3],[1,3,'X',2]))
-  log(render.equals({a:2,b:3},{a:2,b:4}))
-  log(render.equals({a:2,b:3},{a:2,b:3, c:3, d:3}))
-  log(render.equals({a:2,b:{x:3}},{a:2,b:{x:30}}))
-  log(render.equals({a:2,b:{}},{a:2,b:{x:30}}))
-}
-
